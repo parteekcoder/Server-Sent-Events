b5d4a64d7e51d715cac7316d8d23384131dbb166
Parteek Goyal <parteekgoyal118@gmail.com>
2024-11-08 22:25:51 +0530

    added more server events

--- server.js
+++ server.js
@@ -6,13 +6,27 @@ app.get("/", (req,res) => res.sendFile(path.join(__dirname, 'public/index.html')
 app.get("/stream", (req,res) =>{
     res.setHeader("Content-Type","text/event-stream");
     res.write("data: These are the events from server\n\n");
-    send(res)
+    setTimeout(() => sendLoginEvent(res), 1000)
+    setTimeout(()=>  sendNotificationEvent(res),1500)
+    setTimeout(()=>  sendMessage(res),2000)
+    setTimeout(()=>  sendLogoutEvent(res),3000)
 })
 
-let i = 0
-function send(res){
-    res.write("data: " + `Server Event!${i++}\n\n`)
-    setTimeout(()=>send(res),2000)
+function sendLoginEvent(res){
+    res.write("event: Login\n")
+    res.write("data: user loggedin\n\n" )
+}
+function sendMessage(res){
+    res.write("data: Hi this server sent event\n\n" )
+}
+
+function sendNotificationEvent(res){
+    res.write("event: Notify\n")
+    res.write("data: Notification\n\n" )
+}
+function sendLogoutEvent(res){
+    res.write("event: Logout\n")
+    res.write("data: user logged out\n\n" )
 }
 
 app.listen(8080, () => console.log("server started at 8080..."));
